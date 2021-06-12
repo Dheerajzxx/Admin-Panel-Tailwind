60154050d06eba050d55eb3229e28bf32d69f21d
Dheerajzxx <dheerajzxx@gmail.com>
2021-06-12 15:21:30 +0530

    functionality of search and filter added in table

--- public/app.js
+++ public/app.js
@@ -169,6 +169,14 @@ $(document).ready(function () {
       }
     });
 
+    // Table Search
+    $("#table-search").on("keyup", function () {
+      var value = $(this).val().toLowerCase();
+      $("#table-1 tbody tr").filter(function () {
+          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+  });
+
 });
 
 
